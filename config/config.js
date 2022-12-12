1ae37f3b5d622c151bd0bda27e86b45afa55724e
Kyle Davis <kyle.davis@ucdenver.edu>
2022-12-12 15:44:19 -0700

    heroku deployment

---
+++ config/config.js
@@ -0,0 +1,21 @@
+require('dotenv').config()
+module.exports = {
+  development: {
+    database: 'hiker_development',
+    dialect: 'postgres'
+  },
+  test: {
+    database: 'hiker_production',
+    dialect: 'postgres'
+  },
+  production: {
+    use_env_variable: 'DATABASE_URL',
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        rejectUnauthorized: false,
+        require: true
+      }
+    }
+  }
+}
